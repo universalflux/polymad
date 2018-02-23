2a6b8b1f90df77c122100f39a2bff06d8ed63355
Roger Tafoya <rogermasx@gmail.com>
2018-02-23 07:44:01 -0800

    pushing from windows.

--- client/js/index_js.js
+++ client/js/index_js.js
@@ -17,17 +17,17 @@
 	targetWriter.animateCursor(400);
 	}, 600);
 
-	$('#target-two').html("</br>-------- Neural Query => Logic --------- </br> 1. HTML5 </br> 2. CSS3 </br> 3. Javascript </br> 4. Angular 1.5x && AngularJS </br> 5. React </br> 6. NodeJS </br> 7. Express </br> 8. MongoDB </br> 9. GraphQL </br> 10. Ruby</br> 11. RAILS</br>---------- End Query ----------</br>");
+	$('#target-two').html("</br>-------- Neural Skillset => Logic --------- </br><span class='terminal-hover'> 1. HTML5 </span></span></br><span class='terminal-hover'> 2. CSS3 </span></br><span class='terminal-hover'> 3. Javascript </span></br><span class='terminal-hover'> 4. Angular 1.5x && AngularJS </span></br><span class='terminal-hover'> 5. React </span></br><span class='terminal-hover'> 6. NodeJS </span></br><span class='terminal-hover'> 7. Express </span></br><span class='terminal-hover'> 8. MongoDB </span></br><span class='terminal-hover'> 9. GraphQL </span></br><span class='terminal-hover'> 10. Ruby</span></br><span class='terminal-hover'> 11. RAILS</span></br>---------- End Query ----------</span></br>");
 
 	// Just For Fun. Enjoy finding new constellations ;-)
 
 (function($){
-  
+
   function generateStar(canvas, ctx, starRadius){
 			ctx.beginPath();
 			ctx.arc(starRadius + (Math.random() * canvas.width), starRadius + (Math.random() * canvas.height), starRadius*Math.random(), 0, Math.PI*2, false);
       //ctx.arc(100, 30, starRadius, 0, Math.PI*2, false);
-    
+
       var rand = Math.random();
       if(rand <= 0.5){
 				  ctx.fillStyle = "rgba(255, 255, 255, 1)";
@@ -37,7 +37,7 @@
 			else if(rand > 0.75){
 				  ctx.fillStyle = "rgba(255, 254, 196, 1)";
 				  ctx.shadowColor = "rgba(255, 254, 196, 0.5)";
-				  ctx.shadowBlur = 4; 
+				  ctx.shadowBlur = 4;
 			}
 			else{
 				  ctx.fillStyle = "rgba(192, 247, 255, 1)";
@@ -46,18 +46,18 @@
 			}
 			ctx.fill();
 	}
-  
+
   $(function(){
-    
+
 		var canvas = document.getElementById("space");
 		var context = canvas.getContext("2d");
-    
+
     onresize = function(){
       canvas.width = canvas.clientWidth;
       canvas.height = canvas.clientHeight;
     }
     onresize();
-		
+
     interval = setInterval(
       function(interval){
         generateStar(canvas, context, 3);
@@ -68,7 +68,7 @@
       function(){ clearInterval(interval); }
       ,10000
     );
-    
+
 	});
 })(jQuery);
 });
