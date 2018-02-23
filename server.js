d26e1ee74be7f64db1954c34abe47a2761baacc4
Roger Tafoya <rogermasx@gmail.com>
2018-02-23 07:49:14 -0800

    altered server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const io = require("socket.io");
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const path = require('path');
-const port = 2000 || process.env.PORT;
+const port = process.env.PORT || 2000;
 
 app.use(bodyParser.json());
 app.use(morgan('combined'));
@@ -14,4 +14,3 @@ app.use(express.static(path.join(__dirname + '/client')));
 app.listen(port, (req, res) => {
 	console.log("Hello Friend, you are on port#", port);
 });
-
